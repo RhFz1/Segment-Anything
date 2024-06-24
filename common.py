ad1b4bf13c0ad32c2be44f9efed772d87139660a
RhFz1 <nooralifirst@gmail.com>
2024-06-24 13:07:59 +0530

    Implement layer norm for transformer neck

--- common.py
+++ common.py
@@ -20,3 +20,27 @@ class FeedFwd(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.ln2(self.act(self.ln1(x)))
         return x
+
+class LayerNorm2d(nn.Module):
+    """
+        Implementing this to serve a specific case in the neck of the transformer, where we need to normalize the channel dimension.
+        LayerNorm is used to normalize the input tensor on a specific dimension, which is the channel dimension in this case.
+        It has proven to be better than BatchNorm in many cases, as it does not add the noise that BatchNorm does for a batch.
+    """
+
+    def __init__(self,
+                 num_channels:int = 256,
+                 eps: float = 1e-6) -> None:
+        super().__init__()
+        self.ln_gain = nn.Parameter(torch.ones(num_channels))
+        self.ln_bias = nn.Parameter(torch.zeros(num_channels))
+        self.eps = eps
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        B, C, H, W = x.shape
+        x = x.view(B, C, -1)
+        x = (x - x.mean(dim=1, keepdim=True)) * torch.rsqrt(x.var(dim=1, keepdim=True) + self.eps)  # Adding epsilon to avoid division by zero
+       
+        x = x.view(B, C, H, W) # Reshaping back to the original shape
+
+        return x # Normalized tensor
